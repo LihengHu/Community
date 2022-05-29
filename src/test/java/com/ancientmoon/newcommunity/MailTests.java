b9697e88c055f32ca49928304f9db9c56d4a2afe
huliheng <821786679@qq.com>
2022-05-29 23:36:14 +0800

    增加发送邮件功能

---
+++ src/test/java/com/ancientmoon/newcommunity/MailTests.java
@@ -0,0 +1,36 @@
+package com.ancientmoon.newcommunity;
+
+import com.ancientmoon.newcommunity.utils.MailClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+public class MailTests {
+    @Autowired
+    private MailClient mailClient;
+
+    @Resource
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail(){
+        String to = "936933606@qq.com";
+        mailClient.sendMail(to,"hlhnb","胡立恒用java发送的邮件");
+    }
+
+    @Test
+    public void testHtmlMail(){
+        String to = "936933606@qq.com";
+        String SLL= "2251437787@qq.com";
+        Context context = new Context();
+        context.setVariable("username",SLL);
+        String process = templateEngine.process("/mail/demo", context);
+        mailClient.sendMail(SLL,"TEST",process);
+    }
+
+}
