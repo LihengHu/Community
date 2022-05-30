134de934a9ef01e4ab5598baedd43acf8511f48b
huliheng <821786679@qq.com>
2022-05-30 18:35:56 +0800

    完成注册和激活功能

---
+++ src/main/java/com/ancientmoon/newcommunity/utils/CommunityConstant.java
@@ -0,0 +1,10 @@
+package com.ancientmoon.newcommunity.utils;
+
+public interface CommunityConstant {
+
+    int ACTIVATION_SUCCESS = 0;
+
+    int ACTIVATION_REPEAT = 1;
+
+    int ACTIVATION_FAILURE = 2;
+}
