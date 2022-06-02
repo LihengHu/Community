68e5d1d5c42e2ab83eb6f9c28a6b524c67be0ec9
huliheng <821786679@qq.com>
2022-06-02 18:50:37 +0800

    将字典树设置为内部类

--- src/main/java/com/ancientmoon/newcommunity/utils/SensitiveFilter.java
+++ src/main/java/com/ancientmoon/newcommunity/utils/SensitiveFilter.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Component
@@ -17,13 +19,14 @@ public class SensitiveFilter {
     //替换符
     private static final String REPLACEMENT = "***";
 
-    private Trie trie = new Trie();
+    private final Trie trie = new Trie();
 
     @PostConstruct
     public void init(){
         trie.insert("赌博");
         trie.insert("嫖娼");
         trie.insert("吸毒");
+        trie.insert("黄色");
     }
 
     public String filter(String text){
@@ -53,5 +56,59 @@ public class SensitiveFilter {
     }
 
 
+    private static class Trie {
+        public Map<Character, Trie> children;
+        public boolean isEnd;
+
+        Trie(){
+            this.children = new HashMap<>();
+            isEnd = false;
+        }
+
+        public void insert(String word){
+            Trie node = this;
+            for(int i = 0 ;i<word.length() ;i++){
+                char ch = word.charAt(i);
+                if(node.children.get(ch) == null){
+                    node.children.put(ch, new Trie());
+                }
+                node = node.children.get(ch);
+            }
+            node.isEnd = true;
+        }
+
+        public boolean search(String word) {
+            Trie node = this;
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                if(isSymbol(ch)){ continue;}
+                if (node.children.get(ch) == null) {
+                    return false;
+                }
+                node = node.children.get(ch);
+            }
+            return node.isEnd;
+        }
+
+        public boolean searchPrefix(String word) {
+            Trie node = this;
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                if(isSymbol(ch)){
+                    if(i == 0) return false;
+                    continue;
+                }
+                if (node.children.get(ch) == null) {
+                    return false;
+                }
+                node = node.children.get(ch);
+            }
+            return true;
+        }
+
+        private boolean isSymbol(Character ch){
+            return !CharUtils.isAsciiAlphanumeric(ch) && (ch < 0x2E80 || ch> 0x9FFF);
+        }
+    }
 
 }
