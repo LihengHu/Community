126327f2b7c44a6103f9b34c1f142dd91dfd67e3
huliheng <821786679@qq.com>
2022-07-06 17:09:26 +0800

    添加缓存

--- src/main/java/com/ancientmoon/newcommunity/service/DiscussPostService.java
+++ src/main/java/com/ancientmoon/newcommunity/service/DiscussPostService.java
@@ -3,11 +3,20 @@ package com.ancientmoon.newcommunity.service;
 import com.ancientmoon.newcommunity.dao.mapper.DiscussPostMapper;
 import com.ancientmoon.newcommunity.entity.DiscussPost;
 import com.ancientmoon.newcommunity.utils.SensitiveFilter;
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.HtmlUtils;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class DiscussPostService {
@@ -18,11 +27,71 @@ public class DiscussPostService {
     @Autowired
     SensitiveFilter sensitiveFilter;
 
-    public List<DiscussPost> findDisscussPosts(int usrId, int offset, int limit,int orderMode) {
-        return discussPostMapper.selectDiscussPosts(usrId, offset, limit,orderMode);
+    private static final Logger logger = LoggerFactory.getLogger(DiscussPostService.class);
+
+    @Value("${caffeine.posts.max-size}")
+    private int maxSize;
+
+    @Value("${caffeine.posts.expire-seconds}")
+    private int expireSeconds;
+
+    //Caffeine核心接口：Cache,LoadingCache,AsyncLoadingCache
+
+    //帖子列表缓存
+    private LoadingCache<String,List<DiscussPost>> postListCache;
+
+    //帖子总数缓存
+    private LoadingCache<Integer,Integer> postRowsCache;
+
+    @PostConstruct void init(){
+        //初始化帖子列表缓存
+        postListCache = Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expireSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<String, List<DiscussPost>>() {
+                    @Override
+                    public @Nullable List<DiscussPost> load(String key) throws Exception {
+                        if(key == null || key.length() == 0){
+                            throw new IllegalArgumentException("参数错误");
+                        }
+                        String[] params = key.split(":");
+                        if(params == null || params.length != 2){
+                            throw new IllegalArgumentException("参数错误");
+                        }
+                        int offset = Integer.valueOf(params[0]);
+                        int limit = Integer.valueOf(params[1]);
+                        //二级缓存：Redis -> mysql
+                        logger.debug("load post list from DB.");
+                        return discussPostMapper.selectDiscussPosts(0,offset,limit,1);
+                    }
+                });
+        //初始化帖子总数缓存
+        postRowsCache = Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expireSeconds,TimeUnit.SECONDS)
+                .build(new CacheLoader<Integer, Integer>() {
+                    @Override
+                    public @Nullable Integer load(Integer key) throws Exception {
+                        logger.debug("load rows from DB.");
+                        return discussPostMapper.selectDiscussPostRows(key);
+                    }
+                });
+    }
+
+
+    public List<DiscussPost> findDisscussPosts(int userId, int offset, int limit,int orderMode) {
+        if(userId == 0 && orderMode == 1){
+            return postListCache.get(offset+":"+limit);
+        }
+        logger.debug("load post list from DB.");
+        return discussPostMapper.selectDiscussPosts(userId, offset, limit,orderMode);
     }
 
     public int findDiscussPostRows(int userId) {
+        if(userId == 0){
+            return postRowsCache.get(userId);
+        }
+        logger.debug("load post rows from DB");
         return discussPostMapper.selectDiscussPostRows(userId);
     }
 
